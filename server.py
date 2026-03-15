3803ab5c72713294f5dc1c8da17ad9a36facc2f6
Vubni <eimanaev@gmail.com>
2026-03-15 19:06:21 +0300

    fix swagger

--- server.py
+++ server.py
@@ -130,11 +130,18 @@ if __name__ == "__main__":
     if "components" in swagger_dict:
         swagger_dict.setdefault("components", {}).setdefault("securitySchemes", {})["adminBearer"] = admin_bearer_scheme
     else:
-        swagger_dict.setdefault("securityDefinitions", {})["adminBearer"] = {
+        security_definitions = swagger_dict.setdefault("securityDefinitions", {})
+        security_definitions["adminBearer"] = {
             "type": "apiKey",
             "in": "header",
             "name": "Authorization",
             "description": admin_bearer_scheme.get("description", ""),
+        }
+        security_definitions["userBearer"] = {
+            "type": "apiKey",
+            "in": "header",
+            "name": "Authorization",
+            "description": user_bearer_scheme.get("description", ""),
         }
 
     cors.add(app.router.add_route("GET", "/{path:.*}", handle_get_file))
